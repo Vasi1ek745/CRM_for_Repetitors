ce3693b2429b2bebabce92860846f236aa34908d
Vasi1ek745 <vasi1ek745@gmail.com>
2022-07-28 21:09:44 +0500

    end delete edit and show

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,14 +11,41 @@ class StudentsController < ApplicationController
   end
 
   def create
+      @student = Student.new student_params     
+      if @student.save
+        flash[:success] = "Новый ученик добавлен"
+        redirect_to students_path
+      else
+        render :new
+      end
   end
 
   def edit
+    @student = Student.find params[:id]
   end
 
   def update
+    @student = Student.find params[:id]
+    if @student.update student_params
+      flash[:success] = "Данные изменены"
+      redirect_to students_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @student = Student.find params[:id]
+    if @student.delete
+      flash[:success] = "Ученик удален"
+      redirect_to students_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:name, :subject, :location, :pay_by_hour, :other, :class_in_school) 
   end
 end
