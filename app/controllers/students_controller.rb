f78bc5766dbbb4e1847618d36d1eba56224dc0d0
Vasi1ek745 <vasi1ek745@gmail.com>
2022-07-28 16:56:30 +0500

    begin do new student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,12 +1,13 @@
 class StudentsController < ApplicationController
   def index
-    @student = Student.all
+    @students = Student.all
   end
 
   def show
   end
 
   def new
+    @student = Student.new
   end
 
   def create
