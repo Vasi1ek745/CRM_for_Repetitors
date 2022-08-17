0628988731ef93c2ad4ec163a1084072448d0c61
Vasi1ek745 <vasi1ek745@gmail.com>
2022-08-17 18:08:48 +0500

    add Capistrano gem and deploy

--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_04_093123) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
