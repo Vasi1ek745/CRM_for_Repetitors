d1befb60e51c26f21526a75ac919f7ca728f2617
Vasi1ek745 <vasi1ek745@gmail.com>
2022-08-20 17:41:56 +0500

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,8 @@ set :repo_url, "git@github.com:Vasi1ek745/CRM_for_Repetitors.git"
 # Deploy to the user's home directory
 set :deploy_to, "/home/vasiliy/#{fetch :application}"
 
+set :rbenv_prefix, '/usr/bin/rbenv exec'
+
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 
 # Only keep the last 5 releases to save disk space
